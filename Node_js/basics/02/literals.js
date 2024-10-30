f47cb2f020f8b6109f4d47f7a5af7979142aae89
goomil <ss01057734759@gmail.com>
2024-10-30 17:44:34 +0900

    "2024-10-30 Node.js"

---
+++ Node_js/basics/02/literals.js
@@ -0,0 +1,7 @@
+let num01 = 10;
+let num02 = 20;
+
+console.log(num01 + "과" + num02 + " 을 더하면 " + (num01 + num02));
+console.log(`num01: ${num01}, num02: ${num02}, num01+num02: ${num01+num02}`);
+// ES6에서 추가된 템플릿 리터럴인 `(백틱문자(기호)) 를 활용한 경우이다.
+// 변수명을 처리할 때는 ${}를 사용하여 처리 한다.
